a314ca07bb2f0ebfa69749576b0ebc8213574a65
silence28b <boburbekboboniyozov@gmail.com>
2025-08-08 16:36:34 +0500

    we deleted all trash problems. add A0015.java galtaka soldim

--- src/A0015.java
+++ src/A0015.java
@@ -1,2 +1,31 @@
-public class A0015 {ps
+import java.util.Scanner;
+
+public class A0015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int sum = 0;
+        int no = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j]==1){
+                    sum += 1;
+                } else {
+                    no += 1;
+                }
+
+            }
+        }
+        System.out.println(sum / 2);
+
+
+
+
+    }
 }
---
+++ src/A0063.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A0063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int p = sc.nextInt();
+        int[] result = new int[2];
+        for (int x = 0; x <= 1000; x++) {
+            int y = s - x;
+
+            if (y>=1 && x * y ==p ){
+                result[0] = Math.min(x, y);
+                result[1] = Math.max(x, y);
+                break;
+
+            }
+
+        }
+        System.out.println(result[0]+ " " +result[1]);
+
+    }
+}
